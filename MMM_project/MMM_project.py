f358303abac34f6e72388ed2120857fe68928f60
xXMaKuSXx <93930187+xXMaKuSXx@users.noreply.github.com>
2022-05-18 22:44:09 +0200

    Update MMM_project.py

--- MMM_project/MMM_project.py
+++ MMM_project/MMM_project.py
@@ -1,48 +1,71 @@
-﻿from tkinter import *
+﻿from re import I
+from tkinter import *
 from tkinter import messagebox
 import math
-
+import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt1
+import matplotlib.pyplot as plt2
+from array import *
 
 def click_action():
     if vmasa.get().isnumeric() == TRUE and vopor_powietrza.get().isnumeric() == TRUE and vkat_wystrzalu.get().isnumeric() == TRUE and vpredkosc_pocz.get().isnumeric() == TRUE :
         m = float (vmasa.get())
-        k = float (vopor_powietrza.get())
+        b = float (vopor_powietrza.get())
         a = float (vkat_wystrzalu.get())
-        rad = float ((k/180)*math.pi)
         v = float (vpredkosc_pocz.get())
-
-
-        """OBLICZENIA"""
-        b = float (k/m)
-        Vox = float (v*math.cos(rad))
-        g = float (9.81)
-        Voy = float (v*math.sin(rad))
+        t_tab = array('f',[0,0.1])
+        x_tab = array('f',[0,0.1])
+        y_tab = array('f',[0,0.1])
+        x_max = float(0)
+        y_max =float(0)
         x_t = float (0)
         y_t = float (0)
-        t = int (0)
+        t = float (0)
+        t_max_y = float (0)
+        t_max = float (0)
 
+        """OBLICZENIA"""
+        
+        rad = float ((a/180)*math.pi)
+        k = float (b/m)
+        Vox = float (v*math.cos(rad))
+        g = float (9.81)
+        Voy = float (v*math.sin(rad))        
+        
         while y_t >= 0:
-            """os_x = t
-            os_y1 = x_t
-            os_y2 = x_t"""
-            x_t = (Vox/b)*(1 - math.exp(-b*t))
-            y_t = (((Voy/b)+(g/b))*(1 - math.exp(-b*t))) - ((g*t)/b)
-            print (t)
-            print (y_t)
-            print(x_t)
-            t = t + 1
+            x_t = (Vox/k)*(1 - math.exp(-k*t))
+            y_t = (((Voy/k)+(g/b))*(1 - math.exp(-k*t))) - ((g*t)/k)
+            i = int(t*100)
+            t_tab.insert(i,t)
+            x_tab.insert(i,x_t)
+            y_tab.insert(i,y_t)
+            if y_t>y_max:
+                y_max = y_t
+                t_max_y = t
+            elif  x_t>x_max:
+                x_max = x_t
+            t_max = t
+            t = t + 0.01
 
-            
-            
-        print("KONIEC !!")
+        i = int(t*100) 
+        x_tab.insert(i,x_max)
+        y_tab.insert(i,0)
 
+        print("Y max = ")
+        print(y_max)
+        print("Czas do osiągnięcia Y max =")
+        print(t_max_y)
+        print("X max = ")
+        print(x_max)
+        print("T max = ")
+        print(t_max)
+        plt.plot(x_tab,y_tab,color='tab:red')
+        plt.xlim([0,x_t+0.01])
+        plt.ylim([0,y_max+0.01])
+        plt.title('WYKRES y(x)')
+        plt.show()
 
-        print(m)
-        print(k)
-        print(a)
-        print(rad)
-        print(v)
-        print("Wow!")
+        print("KONIEC LOT!!")
     else:
         vmasa.delete(0, END)
         vmasa.insert(END, '0')
@@ -53,14 +76,128 @@ def click_action():
         vpredkosc_pocz.delete(0, END)
         vpredkosc_pocz.insert(END, '0')
         messagebox.showerror('Informacja', 'Źle wprowadzone dane')
-        print("LOL!")
+        print("KONIEC, ZŁE DANE!")
+    
+def click_action1():
+    if vmasa.get().isnumeric() == TRUE and vopor_powietrza.get().isnumeric() == TRUE and vkat_wystrzalu.get().isnumeric() == TRUE and vpredkosc_pocz.get().isnumeric() == TRUE :
+        m = float (vmasa.get())
+        b = float (vopor_powietrza.get())
+        a = float (vkat_wystrzalu.get())
+        v = float (vpredkosc_pocz.get())
+        t_tab = array('f',[0,0.1])
+        x_tab = array('f',[0,0.1])
+        y_tab = array('f',[0,0.1])
+        x_max = float(0)
+        y_max =float(0)
+        x_t = float (0)
+        y_t = float (0)
+        t = float (0)
 
+        """OBLICZENIA"""
+        
+        rad = float ((a/180)*math.pi)
+        k = float (b/m)
+        Vox = float (v*math.cos(rad))
+        g = float (9.81)
+        Voy = float (v*math.sin(rad))        
+        
+        while y_t >= 0:
+            x_t = (Vox/k)*(1 - math.exp(-k*t))
+            y_t = (((Voy/k)+(g/b))*(1 - math.exp(-k*t))) - ((g*t)/k)
+            i = int(t*100)
+            t_tab.insert(i,t)
+            x_tab.insert(i,x_t)
+            y_tab.insert(i,y_t)
+            if y_t>y_max:
+                y_max = y_t
+            elif  x_t>x_max:
+                x_max = x_t
+            t = t + 0.01
 
+        i = int(t*100) 
+        x_tab.insert(i,x_max)
+        t_tab.insert(i,t)       
+        plt1.plot(t_tab,x_tab,color='tab:red')
+        plt1.xlim([0,t+0.01])
+        plt1.ylim([0,x_max+0.01])
+        plt1.title('WYKRES x(t)')
+        plt1.show()
+
+        print("KONIEC x(t)!")
+    else:
+        vmasa.delete(0, END)
+        vmasa.insert(END, '0')
+        vopor_powietrza.delete(0, END)
+        vopor_powietrza.insert(END, '0')
+        vkat_wystrzalu.delete(0, END)
+        vkat_wystrzalu.insert(END, '0')
+        vpredkosc_pocz.delete(0, END)
+        vpredkosc_pocz.insert(END, '0')
+        messagebox.showerror('Informacja', 'Źle wprowadzone dane')
+        print("KONIEC ZŁE DANE!")
+
+def click_action2():
+    if vmasa.get().isnumeric() == TRUE and vopor_powietrza.get().isnumeric() == TRUE and vkat_wystrzalu.get().isnumeric() == TRUE and vpredkosc_pocz.get().isnumeric() == TRUE :
+        m = float (vmasa.get())
+        b = float (vopor_powietrza.get())
+        a = float (vkat_wystrzalu.get())
+        v = float (vpredkosc_pocz.get())
+        t_tab = array('f',[0,0.1])
+        x_tab = array('f',[0,0.1])
+        y_tab = array('f',[0,0.1])
+        x_max = float(0)
+        y_max =float(0)
+        x_t = float (0)
+        y_t = float (0)
+        t = float (0)
+
+        """OBLICZENIA"""
+        
+        rad = float ((a/180)*math.pi)
+        k = float (b/m)
+        Vox = float (v*math.cos(rad))
+        g = float (9.81)
+        Voy = float (v*math.sin(rad))    
+        
+        while y_t >= 0:
+            x_t = (Vox/k)*(1 - math.exp(-k*t))
+            y_t = (((Voy/k)+(g/b))*(1 - math.exp(-k*t))) - ((g*t)/k)
+            i = int(t*100)
+            t_tab.insert(i,t)
+            x_tab.insert(i,x_t)
+            y_tab.insert(i,y_t)
+            if y_t>y_max:
+                y_max = y_t
+            elif  x_t>x_max:
+                x_max = x_t
+            t = t + 0.01
+
+        i = int(t*100) 
+        y_tab.insert(i,0)
+        t_tab.insert(i,t)
+        plt2.plot(t_tab,y_tab,color='tab:red')
+        plt2.xlim([0,t+0.01])
+        plt2.ylim([0,y_max+0.01])
+        plt2.title('WYKRES y(t)')
+        plt2.show()
+
+        print("KONIEC y(t)!")
+    else:
+        vmasa.delete(0, END)
+        vmasa.insert(END, '0')
+        vopor_powietrza.delete(0, END)
+        vopor_powietrza.insert(END, '0')
+        vkat_wystrzalu.delete(0, END)
+        vkat_wystrzalu.insert(END, '0')
+        vpredkosc_pocz.delete(0, END)
+        vpredkosc_pocz.insert(END, '0')
+        messagebox.showerror('Informacja', 'Źle wprowadzone dane')
+        print("KONIEC ZŁE DANE!")
 
 
 root = Tk()
 root.title("MMM")
-root.geometry("900x800")
+root.geometry("800x600")
 text_label = Label(root, font=15, text="Wprowadź parametry")
 masa = Label(root, font=40, text="Masa  [kg]")
 opor_powietrza = Label(root, font=40, text="Opór powietrza [kg/s]")
@@ -68,119 +205,30 @@ kat_wystrzalu = Label(root, font=40, text="Kąt wystrzału [°]")
 predkosc_pocz = Label(root, font=40, text="Prędkość początkowa [m/s]")
 text_label.pack()
 
-masa.place(x=0,y=50,height=50,width=150)
+masa.place(x=0,y=50,height=50,width=200)
 vmasa = Entry(root, width=10)
 vmasa.insert(END, '0')
-vmasa.place(x=10,y=100,height=25,width=130)
+vmasa.place(x=10,y=100,height=25,width=180)
 
-opor_powietrza.place(x=230,y=50,height=50,width=180)
+opor_powietrza.place(x=200,y=50,height=50,width=200)
 vopor_powietrza = Entry(root,width=10) 
 vopor_powietrza.insert(END, '0')
-vopor_powietrza.place(x=240,y=100,height=25,width=130)
+vopor_powietrza.place(x=210,y=100,height=25,width=180)
 
-kat_wystrzalu.place(x=460,y=50,height=50,width=150)
+kat_wystrzalu.place(x=400,y=50,height=50,width=200)
 vkat_wystrzalu = Entry(root,width=10)
 vkat_wystrzalu.insert(END, '0')
-vkat_wystrzalu.place(x=470,y=100,height=25,width=130)
+vkat_wystrzalu.place(x=410,y=100,height=25,width=180)
 
-predkosc_pocz.place(x=670,y=50,height=50,width=250)
+predkosc_pocz.place(x=600,y=50,height=50,width=200)
 vpredkosc_pocz = Entry(root,width=10)
 vpredkosc_pocz.insert(END, '0')
-vpredkosc_pocz.place(x=700,y=100,height=25,width=130)
+vpredkosc_pocz.place(x=610,y=100,height=25,width=180)
 
-Button(root, text="Zatwierdź", font=40, width=10, command=click_action).place(x=385,y=150,height=50,width=150)
+Button(root, text="LOT POCISKU", font=40, width=10, command=click_action).place(x=325,y=150,height=50,width=150)
+Button(root, text="RZUT OŚ X", font=40, width=10, command=click_action1).place(x=150,y=150,height=50,width=150)
+Button(root, text="RZUT OŚ Y", font=40, width=10, command=click_action2).place(x=500,y=150,height=50,width=150)
+
 
 
 root.mainloop()
-
-'''
-from tkinter import *
-from tkinter import messagebox as msg
- 
- 
-def gui2_init():
-    gui2 = Tk()
-    gui_settings(gui2)
-    gui2.mainloop()
- 
- 
-def stability_test(k, a, A, m, gui):
-    if a > 0 and k * A * m > 0:
-        print("stability")
-        gui.destroy()
-        gui2_init()
-    else:
-        msg.showerror(title="!!!", message="Układ jest niestabilny")
-        return 0
- 
- 
-def submit(k_entry, a_entry, A_entry, m_entry, gui):
-    # get value
-    try:
-        k = float(k_entry.get())
-        a = float(a_entry.get())
-        A = float(A_entry.get())
-        m = float(m_entry.get())
- 
-        if k < 0:
-            msg.showerror(title="!!!", message="Wartość k musi być dodatnia")
- 
-        if A < 0:
-            msg.showerror(title="!!!", message="Wartość k musi być dodatnia")
- 
-        if stability_test(k, a, A, m, gui) == 0:
-            # RESET ENTRY
-            k_entry.delete(0, END)
-            a_entry.delete(0, END)
-            A_entry.delete(0, END)
-            m_entry.delete(0, END)
- 
-    except:
-        msg.showerror(title="!!!", message="Wpisz wartości")
- 
- 
-def gui_settings(gui):
-    gui.configure(bg="lightgray")
-    gui.title("MMM")
-    gui.geometry("900x400")
- 
- 
-def gui_init():
-    # INIT tk
-    gui = Tk()
-    gui_settings(gui)
- 
- 
-    Label(gui, text="Podaj wartość k:", font="Helvetica 10 bold", bg="lightgray").grid(row=1, column=0, padx=20,
-                                                                                       pady=0)
-    Label(gui, text="Podaj wartość a:", font="Helvetica 10 bold", bg="lightgray").grid(row=1, column=1, padx=20,
-                                                                                       pady=0)
-    Label(gui, text="Podaj wartość A:", font="Helvetica 10 bold", bg="lightgray").grid(row=1, column=2, padx=20,
-                                                                                       pady=0)
-    Label(gui, text="Podaj wartość m:", font="Helvetica 10 bold", bg="lightgray").grid(row=1, column=3, padx=20,
-                                                                                       pady=0)
- 
-    k_entry = Entry(gui, width=30)
-    k_entry.grid(row=2, column=0, padx=20, pady=0)
- 
-    a_entry = Entry(gui, width=30)
-    a_entry.grid(row=2, column=1, padx=20, pady=0)
- 
-    A_entry = Entry(gui, width=30)
-    A_entry.grid(row=2, column=2, padx=20, pady=0)
- 
-    m_entry = Entry(gui, width=30)
-    m_entry.grid(row=2, column=3, padx=20, pady=0)
- 
-    button_submit = Button(gui, text="Submit", bg="gray", height=2, width=15,
-                           command=lambda: submit(k_entry, a_entry, A_entry, m_entry, gui))
-    button_submit.grid(row=3, column=0, padx=5, pady=10, columnspan=5)
- 
-    # START
-    gui.mainloop()
- 
- 
-if __name__ == '__main__':
-    gui_init()
-
-    '''
